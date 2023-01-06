592cbaa71b1547ff857209afb4d54430baedfe30
Ian Lasky <ianmlasky@gmail.com>
2023-01-06 10:19:27 -0500

    initial commit

---
+++ indiecomp/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from indiecomp.users.models import User
+from indiecomp.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
