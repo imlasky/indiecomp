2b798920a5e8798797d37762cc7e6bf7c8c10357
Ian Lasky <ianmlasky@gmail.com>
2023-01-07 09:32:41 -0500

    remove job func

--- indiecomp/jobs/urls.py
+++ indiecomp/jobs/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from indiecomp.jobs.views import JobListView, apply
+from indiecomp.jobs.views import JobListView, apply, remove_job_from_user
 
 app_name = "jobs"
 urlpatterns = [
     path("", view=JobListView.as_view(), name="job_list"),
     path("jobs/<uuid:pk>/apply/", view=apply, name="apply"),
+    path("jobs/<uuid:pk>/remove_from_user/", view=remove_job_from_user, name="remove_job_from_user")
 ]
--- indiecomp/jobs/views.py
+++ indiecomp/jobs/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import get_object_or_404, redirect
 from django.views.generic.list import ListView
+from django.contrib.auth import get_user_model
 
 from indiecomp.jobs.models import Job
 
+User = get_user_model()
 
 class JobListView(ListView):
     model = Job
@@ -16,5 +18,19 @@ def apply(request, pk):
         job = get_object_or_404(Job, pk=pk)
         job.num_apply += 1
         job.save()
+        if request.user.is_authenticated:
+            user = get_object_or_404(User, id=request.user.id)
+            user.saved_jobs.add(job)
+            user.save()
         return redirect(job.application_url)
+    return redirect("/")
+
+def remove_job_from_user(request, pk):
+    if request.method == "POST":
+        job = get_object_or_404(Job, pk=pk)
+        if request.user.is_authenticated:
+            user = get_object_or_404(User, id=request.user.id)
+            user.saved_jobs.remove(job)
+            user.save()
+        return redirect(f"/users/{user.username}")
     return redirect("/")
